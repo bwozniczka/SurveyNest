d84030ef758c863dc9932216e5f543f5ad0b3ebc
Bartłomiej Woźniczka <wozniczka.bart@gmail.com>
2024-11-14 15:51:18 +0100

    pricing page change

--- app/[locale]/(main)/pricing/page.tsx
+++ app/[locale]/(main)/pricing/page.tsx
@@ -1,8 +1,31 @@
 import { pricingPlans } from "@/constants";
 import SubscriptionPlan from "@/components/SubscriptionCard";
 import Navbar from "@/components/Navbar";
+import { auth } from "@/auth";
 
-export default function PricingPage() {
+export default async function PricingPage() {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <>
+        <Navbar />
+        <div className="flex flex-wrap justify-center items-center gap-6 min-h-screen">
+          {pricingPlans.map((plan) => (
+            <SubscriptionPlan
+              key={plan.id}
+              title={plan.title}
+              price={plan.price}
+              billingPeriod={plan.billingPeriod}
+              features={plan.features}
+              buttonText={"Sign in"}
+              href={"/signin"}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <Navbar />
