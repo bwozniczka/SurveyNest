081e9b69bf55a5f7ba7d6158ac1e8238a0e221fe
Jakub Wiechniak <wiechniak.j@gmail.com>
2025-01-10 15:22:35 +0100

    merge with templates

---
+++ app/templates/event-registration/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import EventRegistrationForm from "@/components/survey-templates/EventRegistrationForm";
+
+export default function EventRegistrationPage() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <EventRegistrationForm></EventRegistrationForm>
+        </>
+    );
+}
--- constants/index.ts
+++ constants/index.ts
@@ -29,37 +29,37 @@ export const components: {
   href: string;
   description: string;
 }[] = [
-  {
-    title: "Event Registration",
-    href: "/templates/event-registration",
-    description:
-      "A form that allows users to register for an event, providing their contact information and any other required details.",
-  },
-  {
-    title: "Feedback Form",
-    href: "/templates/feedback-form",
-    description:
-      "A form that allows users to provide feedback on a product or service.",
-  },
-  {
-    title: "Job Application",
-    href: "/templates/job-application",
-    description:
-      "A form that allows users to apply for a job, providing their contact information, resume, and any other required details.",
-  },
-  {
-    title: "Newsletter Signup",
-    href: "/templates/newsletter-signup",
-    description:
-      "A form that allows users to sign up for a newsletter, providing their contact information.",
-  },
-  {
-    title: "Contact Form",
-    href: "/templates/contact-form",
-    description:
-      "A form that allows users to contact a business or organization, providing their contact information and message.",
-  },
-];
+    {
+      title: "Event Registration",
+      href: "/templates/event-registration",
+      description:
+        "A form that allows users to register for an event, providing their contact information and any other required details.",
+    },
+    {
+      title: "Feedback Form",
+      href: "/templates/feedback-form",
+      description:
+        "A form that allows users to provide feedback on a product or service.",
+    },
+    {
+      title: "Job Application",
+      href: "/templates/job-application",
+      description:
+        "A form that allows users to apply for a job, providing their contact information, resume, and any other required details.",
+    },
+    {
+      title: "Newsletter Signup",
+      href: "/templates/newsletter-signup",
+      description:
+        "A form that allows users to sign up for a newsletter, providing their contact information.",
+    },
+    {
+      title: "Contact Form",
+      href: "/templates/contact-form",
+      description:
+        "A form that allows users to contact a business or organization, providing their contact information and message.",
+    },
+  ];
 
 export const pricingPlans = [
   {
