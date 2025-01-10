7aafd52532c97c859c9a273c539025ad8949f1d0
Jakub Wiechniak <wiechniak.j@gmail.com>
2025-01-10 15:16:07 +0100

    first form templates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,8 @@ import { Highlight } from "./ui/hero-highlight";
 
 export function Hero() {
   return (
-    <div className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-dark max-w-5xl leading-relaxed lg:leading-snug text-center mx-auto overflow-x-auto">
-      Revolutionize Your Surveys with SurveyNest{" "}
+    <div className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-dark max-w-6xl leading-relaxed lg:leading-snug text-center mx-auto overflow-x-auto">
+      Revolutionize Your Surveys with SurveyNest<br />{" "}
       <Highlight className="text-black dark:text-white block">
         Innovative, Fast, and User-Friendly!
       </Highlight>
